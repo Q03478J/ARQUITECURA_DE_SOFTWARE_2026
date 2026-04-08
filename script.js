260dad6ad21a2da1898202e4ecc0cbfd916ad088
Q03478J <q03478j@upla.edu.pe>
2026-04-08 00:51:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function login() {
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
 
+    if (pass === "123") {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("panel").style.display = "block";
+    } else {
+        alert("Contraseña incorrecta");
+    }
+}
+
+function verSemana(num) {
+    document.getElementById("contenido").innerHTML =
+        "<h2>Semana " + num + "</h2><p>Aquí irá tu contenido</p>";
+}
