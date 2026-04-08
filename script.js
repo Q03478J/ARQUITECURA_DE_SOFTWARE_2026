1f4b84d47f9e3651832a3662e6de0a9e3b984763
Q03478J <q03478j@upla.edu.pe>
2026-04-08 00:55:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-function login() {
-    let user = document.getElementById("user").value;
-    let pass = document.getElementById("pass").value;
-
-    if (pass === "123") {
-        document.getElementById("login").style.display = "none";
-        document.getElementById("panel").style.display = "block";
-    } else {
-        alert("Contraseña incorrecta");
-    }
+function mostrarInicio() {
+    document.getElementById("contenido").innerHTML =
+        "<h1>Bienvenido</h1><p>Este es el curso de Arquitectura de Software</p>";
 }
 
 function verSemana(num) {
     document.getElementById("contenido").innerHTML =
-        "<h2>Semana " + num + "</h2><p>Aquí irá tu contenido</p>";
+        "<h2>Semana " + num + "</h2><p>Aquí irá tu contenido de la semana</p>";
+}
+
+function mostrarLogin() {
+    document.getElementById("login").style.display = "block";
+}
+
+function login() {
+    let pass = document.getElementById("pass").value;
+
+    if (pass === "123") {
+        alert("Bienvenido");
+        document.getElementById("login").style.display = "none";
+    } else {
+        alert("Contraseña incorrecta");
+    }
 }
